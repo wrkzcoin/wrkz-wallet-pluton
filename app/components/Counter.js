7e3031be07881b39f632c07434f69a8f254ecc2f
ExtraHash <extrahash@protonmail.com>
2019-06-07 21:44:31 -0600

    continue writing receive page

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -1,9 +1,8 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import styles from './Counter.css';
 import routes from '../constants/routes';
-import { WalletBackend } from 'turtlecoin-wallet-backend';
+// import styles from './Counter.css';
 
 type Props = {
   increment: () => void,
@@ -26,43 +25,48 @@ export default class Counter extends Component<Props> {
     } = this.props;
     return (
       <div>
-        <nav className="navbar" role="navigation" aria-label="main navigation">
-          <div className="navbar-brand">
-            <Link to={routes.HOME}>
-              <a className="navbar-item" href="#">
-                <img
-                  src="./img/trtl-logo.png"
-                  alt="logo"
-                  className="img-responsive"
-                />
-              </a>
-            </Link>
-            <Link to={routes.HOME}>
-              <a className="navbar-item">History</a>
-            </Link>
-            <Link to={routes.COUNTER}>
-              <a className="navbar-item">Send</a>
-            </Link>
-            <Link to={routes.COUNTER}>
-              <a className="navbar-item">Receive</a>
-            </Link>
-          </div>
-        </nav>
-        <br />
         <div>
-          <div className="field is-horizontal">
-            <div className="field-label is-normal">
-              <label className="label">Receive Address:</label>
+          <nav
+            className="navbar"
+            role="navigation"
+            aria-label="main navigation"
+          >
+            <div className="navbar-brand">
+              <Link to={routes.HOME}>
+                <a className="navbar-item" href="#">
+                  <img
+                    src="./img/trtl-logo.png"
+                    alt="logo"
+                    className="img-responsive"
+                  />
+                </a>
+              </Link>
+              <Link to={routes.HOME}>
+                <a className="navbar-item">History</a>
+              </Link>
+              <Link to={routes.COUNTER}>
+                <a className="navbar-item">Send</a>
+              </Link>
+              <Link to={routes.COUNTER}>
+                <a className="navbar-item">Receive</a>
+              </Link>
             </div>
-            <div className="field-body">
-              <div className="field">
-                <p className="control">
-                  <input className="input" type="receive-address" value={window.session.address} readonly />
-                </p>
-              </div>
+          </nav>
+        </div>
+        <br />
+        <section>
+          <div className="container is-fluid">
+            <div className="notification">
+              <h1>Receiving Address</h1>
+              <textarea
+                className="textarea is-success"
+                placeholder={window.session.address}
+                readOnly
+              />
+              <button type="button" className="button is-success">Copy to Clipboard</button>
             </div>
           </div>
-        </div>
+        </section>
       </div>
     );
   }
