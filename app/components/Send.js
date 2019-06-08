6ba22837d98dbd1b3126ba8848f19aa48a32f641
ExtraHash <ExtraHash@protonmail.com>
2019-06-08 11:00:59 -0600

    update send page

--- app/components/Send.js
+++ app/components/Send.js
@@ -13,34 +13,95 @@ export default class Send extends Component<Props> {
     const {} = this.props;
     return (
       <div>
-        <div>
-          <nav
-            className="navbar"
-            role="navigation"
-            aria-label="main navigation"
-          >
-            <div className="navbar-brand">
-              <Link to={routes.HOME}>
-                <a className="navbar-item" href="#">
-                  <img
-                    src="./img/trtl-logo.png"
-                    alt="logo"
-                    className="img-responsive"
-                  />
-                </a>
-              </Link>
-              <Link to={routes.HOME}>
-                <a className="navbar-item">History</a>
-              </Link>
-              <Link to={routes.SEND}>
-                <a className="navbar-item is-active">Send</a>
-              </Link>
-              <Link to={routes.COUNTER}>
-                <a className="navbar-item">Receive</a>
-              </Link>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <Link to={routes.HOME}>
+              <a className="navbar-item" href="#">
+                <img
+                  src="./img/trtl-logo.png"
+                  alt="logo"
+                  className="img-responsive"
+                />
+              </a>
+            </Link>
+            <Link to={routes.HOME}>
+              <a className="navbar-item">History</a>
+            </Link>
+            <Link to={routes.SEND}>
+              <a className="navbar-item is-active">Send</a>
+            </Link>
+            <Link to={routes.COUNTER}>
+              <a className="navbar-item">Receive</a>
+            </Link>
+          </div>
+        </nav>
+        <section>
+          <div className="container is-fluid">
+            <div className="notification">
+              <div className="field">
+                <label className="label" htmlFor="address">
+                  Send to Address
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="Enter a TurtleCoin address to send funds to."
+                      id="label"
+                    />
+                  </div>
+                </label>
+              </div>
+              <div className="field">
+                <label className="label" htmlFor="paymentid">
+                  Payment ID (Optional)
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="Enter a payment ID"
+                      id="paymentid"
+                    />
+                  </div>
+                </label>
+              </div>
+              <div className="field">
+                <label className="label" htmlFor="amount">
+                  Amount
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="How much TRTL to send (eg. 100)"
+                      id="amount"
+                    />
+                  </div>
+                </label>
+              </div>
+              <div className="field">
+                <label className="label" htmlFor="fee">
+                  Fee (Optional)
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="Enter desired mining fee"
+                      id="fee"
+                    />
+                  </div>
+                  <p className="help">
+                    This will default to the lowest possible fee (0.1 TRTL)
+                  </p>
+                </label>
+              </div>
+              <button type="submit" className="button is-success">
+                Send
+              </button>
+              <button type="submit" className="button">
+                Clear
+              </button>
             </div>
-          </nav>
-        </div>
+          </div>
+        </section>
       </div>
     );
   }
