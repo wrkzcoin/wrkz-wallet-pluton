959ef3fdd363d88200d1a95dad5aea6a9582321f
ExtraHash <extrahash@protonmail.com>
2019-06-09 10:55:27 -0600

    adjust some string concat to using templates and add back in commit linting

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -5,18 +5,25 @@ import fs from 'fs';
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import counter from './counter';
-import { WalletSession } from '../wallet/session';
-import config  from '../constants/config';
+import WalletSession from '../wallet/session';
+import config from '../constants/config';
 
 const homedir = os.homedir();
-const directories = [homedir + '/.protonwallet', homedir + '/.protonwallet/logs', homedir + '/.protonwallet/wallets'];
+const directories = [
+  `${homedir}/.protonwallet`,
+  `${homedir}/.protonwallet/logs`,
+  `${homedir}/.protonwallet/wallets`
+];
 window.directories = directories;
+// eslint-disable-next-line no-unused-vars
 const [programDirectory, logDirectory, walletDirectory] = directories;
 log.debug('Checking if program directories are present...');
 directories.forEach(function(dir) {
-  if (!fs.existsSync(dir)){
-      log.debug('No directories detected, initial startup detected. Running setup...')
-      fs.mkdirSync(dir);
+  if (!fs.existsSync(dir)) {
+    log.debug(
+      'No directories detected, initial startup detected. Running setup...'
+    );
+    fs.mkdirSync(dir);
   }
 });
 
@@ -27,11 +34,14 @@ window.session = new WalletSession();
 window.session.wallet.start();
 log.debug('Initialized wallet session ', window.session.address);
 
-let walletLogStream = fs.createWriteStream(logDirectory + '/divinewallet.log', {
+const walletLogStream = fs.createWriteStream(`${logDirectory}/divinewallet.log`, {
   flags: 'a'
 });
 
-window.session.wallet.saveWalletToFile(walletDirectory + '/proton.wallet', 'hunter2');
+window.session.wallet.saveWalletToFile(
+  `${walletDirectory}/proton.wallet`,
+  'hunter2'
+);
 
 export default function createRootReducer(history: History) {
   return combineReducers<{}, *>({
