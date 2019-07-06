9d7dd00186c330e68490beb55465a67b14227651
ender <ender@enders-Mac.local>
2019-07-06 17:22:46 -0600

    quit on exit for now, need to figure out new window behavior on mac later

--- app/main.dev.js
+++ app/main.dev.js
@@ -55,11 +55,7 @@ const installExtensions = async () => {
  */
 
 app.on('window-all-closed', () => {
-  // Respect the OSX convention of having the application in memory even
-  // after all windows have been closed
-  if (process.platform !== 'darwin') {
     app.quit();
-  }
 });
 
 contextMenu({
