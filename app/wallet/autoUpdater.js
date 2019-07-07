9b8620f4e5b1c42f3a619471849b61e5e3576653
ExtraHash <ExtraHash@protonmail.com>
2019-07-07 01:40:10 -0600

    fix a bug in the update check

--- app/wallet/autoUpdater.js
+++ app/wallet/autoUpdater.js
@@ -7,24 +7,31 @@ import os from 'os';
 import { eventEmitter } from '../index';
 import npmPackage from '../../package.json';
 
+const currentVersion = npmPackage.version;
 const operatingSystem = os.platform();
+log.debug(operatingSystem);
 
 export default class AutoUpdater {
   getLatestVersion() {
     const options = {
       method: 'GET',
-      url: 'http://68.183.53.229:3000/latest',
+      url: `http://68.183.53.229:3000/latest/${operatingSystem}/${currentVersion}`,
       json: true
     };
     request(options, function(error, response, body) {
-      if (error) throw new Error(error);
+      if (error) {
+        log.debug('Error when contacting update server...');
+        return;
+      }
+      log.debug(body);
       if (semver.gt(body.latestVersion, npmPackage.version)) {
         log.debug(
           `Update required! Local version: ${
             npmPackage.version
           }, latest version: ${body.latestVersion}`
         );
-        let updateFile;
+        const updateFile = body.downloadPath;
+        /*
         if (operatingSystem === 'linux') {
           updateFile = body.downloadLinux;
         } else if (operatingSystem === 'win32') {
@@ -36,6 +43,7 @@ export default class AutoUpdater {
             'Unsupported operating system for automatic updates. Please see build instructions at https://github.com/turtlecoin/turtle-wallet-proton#readme'
           );
         }
+        */
         eventEmitter.emit('updateRequired', updateFile);
       }
       return body;
