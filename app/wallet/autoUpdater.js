508cddc52a1bd1798434167c52e583b0dcb81afa
ExtraHash <ExtraHash@protonmail.com>
2019-07-09 20:29:38 -0600

    don't check for updates on dev environment

--- app/wallet/autoUpdater.js
+++ app/wallet/autoUpdater.js
@@ -12,29 +12,33 @@ const operatingSystem = os.platform();
 
 export default class AutoUpdater {
   getLatestVersion() {
-    log.debug('Checking for updates...');
-    const options = {
-      method: 'GET',
-      url: `http://68.183.53.229:3000/latest/${operatingSystem}/${currentVersion}`,
-      json: true
-    };
-    request(options, function(error, response, body) {
-      if (error) {
-        log.debug('Error when contacting update server...');
-        return;
-      }
-      if (semver.gt(body.latestVersion, npmPackage.version)) {
-        log.debug(
-          `Update required! Local version: ${
-            npmPackage.version
-          }, latest version: ${body.latestVersion}`
-        );
-        const updateFile = body.downloadPath;
-        eventEmitter.emit('updateRequired', updateFile);
-      } else {
-        log.debug('No update found.');
-      }
-      return body;
-    });
+    if (process.env.NODE_ENV !== 'development') {
+      log.debug('Checking for updates...');
+      const options = {
+        method: 'GET',
+        url: `http://68.183.53.229:3000/latest/${operatingSystem}/${currentVersion}`,
+        json: true
+      };
+      request(options, function(error, response, body) {
+        if (error) {
+          log.debug('Error when contacting update server...');
+          return;
+        }
+        if (semver.gt(body.latestVersion, npmPackage.version)) {
+          log.debug(
+            `Update required! Local version: ${
+              npmPackage.version
+            }, latest version: ${body.latestVersion}`
+          );
+          const updateFile = body.downloadPath;
+          eventEmitter.emit('updateRequired', updateFile);
+        } else {
+          log.debug('No update found.');
+        }
+        return body;
+      });
+    } else {
+      log.debug('Development environment detected.');
+    }
   }
 }
