dcb03fa97d28816244ef034a72254e596438d191
ExtraHash <extrahash@protonmail.com>
2019-07-18 10:20:38 -0400

    remove unnecessary log

--- app/index.js
+++ app/index.js
@@ -24,7 +24,6 @@ export function savedInInstallDir(savePath) {
     remote.app.getAppPath(),
     '../../'
   );
-  log.debug(installationDirectory);
   const saveAttemptDirectory = path.resolve(savePath, '../');
   if (
     saveAttemptDirectory === installationDirectory &&
