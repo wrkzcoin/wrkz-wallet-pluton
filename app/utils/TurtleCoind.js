fa167fce94823d2f572a4054df7db5d386a43b52
ExtraHash <ExtraHash@protonmail.com>
2019-09-21 05:59:32 -0600

    windows is a special snowflake

--- app/utils/TurtleCoind.js
+++ app/utils/TurtleCoind.js
@@ -3,11 +3,13 @@
 // Copyright (C) 2019 ExtraHash
 //
 // Please see the included LICENSE file for more information.
-import * as child from 'child_process';
+import child from 'child_process';
 import os from 'os';
 import path from 'path';
+import log from 'electron-log';
 
 const homedir = os.homedir();
+const operatingSystem = os.platform();
 
 const directories = [
   `${homedir}/.protonwallet`,
@@ -21,32 +23,30 @@ export default class TurtleCoind {
 
   arguments: string;
 
+  logPath: string;
+
   turtleCoindPath: string;
 
   err: Error | null;
 
   constructor(turtleCoindPath: string) {
     this.path = turtleCoindPath;
-    this.arguments = `--log-file ${path.resolve(
-      directories[1],
-      'TurtleCoind.log'
-    )}`;
+    this.arguments = `--log-file`;
+    this.logPath = path.resolve(directories[1], 'TurtleCoind.log');
     this.err = null;
-    this.child = child.exec(
-      `${this.path} ${this.arguments}`,
-      { maxBuffer: 1024 * 500, windowsHide: true },
-      // eslint-disable-next-line no-unused-vars
-      (err, stdout, stderr) => {
-        // some stuff here
-        if (err) {
-          throw err;
-        }
-      }
-    );
+    this.child = child.spawn(this.path, [this.arguments, this.logPath], {
+      maxBuffer: 1024 * 500,
+      windowsHide: true
+    });
+    log.debug(this.child.pid);
     this.quit = this.quit.bind(this);
   }
 
   quit = async () => {
-    this.child.kill();
+    if (operatingSystem === 'win32') {
+      child.spawn('TASKKILL', ['/PID', this.child.pid]);
+    } else {
+      this.child.kill('SIGTERM');
+    }
   };
 }
