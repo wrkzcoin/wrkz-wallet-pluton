2ef240fd1a28d500f9bc326b478c9e0ad3eca18d
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2019-12-03 12:54:24 +0700

    Duplicate nodeListURL

--- app/Configure.js
+++ app/Configure.js
@@ -172,8 +172,6 @@ const Configure = new function() {
     this.GitHubRepo = 'https://github.com/wrkzcoin/wrkz-wallet-pluton';
 
     this.DiscordURL = 'https://chat.wrkz.work';
-
-    this.nodeListURL = 'https://node.wrkz.work/list';
 };
 
 export default Configure;
