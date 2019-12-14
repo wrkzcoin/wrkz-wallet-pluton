56daf49b4053ef6cf82b01d7178d9256bc084998
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2019-12-15 06:01:17 +0700

    Add connected node to bottom bar

--- app/mainWindow/components/NodeFee.js
+++ app/mainWindow/components/NodeFee.js
@@ -13,7 +13,8 @@ type Props = {
 };
 
 type State = {
-  nodeFee: number
+  nodeFee: number,
+  connectednode: string
 };
 
 export default class NodeFee extends Component<Props, State> {
@@ -24,7 +25,8 @@ export default class NodeFee extends Component<Props, State> {
   constructor(props?: Props) {
     super(props);
     this.state = {
-      nodeFee: session.getNodeFee()
+      nodeFee: session.getNodeFee() || 0,
+	  connectednode: session.daemon.host + ':' + session.daemon.port
     };
     this.handleNewNodeFee = this.handleNewNodeFee.bind(this);
   }
@@ -39,19 +41,21 @@ export default class NodeFee extends Component<Props, State> {
 
   handleNewNodeFee = () => {
     this.setState({
-      nodeFee: session.getNodeFee()
+      nodeFee: session.getNodeFee(),
+      connectednode: session.daemon.host + ':' + session.daemon.port,
     });
   };
 
   render() {
     const { darkMode, size } = this.props;
-    const { nodeFee } = this.state;
+    const { nodeFee, connectednode } = this.state;
     const color = darkMode ? 'is-dark' : 'is-white';
 
     if (nodeFee > 0) {
       return (
         <div className="control statusicons">
           <div className="tags has-addons">
+            <span className={`tag is-success ${size}`}>{connectednode}</span>
             <span className={`tag ${color} ${size}`}>{il8n.node_fee}</span>
             <span className={`tag is-danger ${size}`}>
               {atomicToHuman(nodeFee, true)} {Configure.ticker}
