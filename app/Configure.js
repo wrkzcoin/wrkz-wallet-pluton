37e7a84e75ce2a4cb23ad9f04002177deeeb790c
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2020-05-19 17:51:39 +0700

    update mixin

--- app/Configure.js
+++ app/Configure.js
@@ -108,7 +108,10 @@ const Configure = new function() {
         /* At height of 658,500 */
         new MixinLimit(658500, 1, 3, 1),
 
-    ], 3 /* Default mixin of 3 before block 440,000 */);
+        /* At height of 1,000,100 */
+        new MixinLimit(1000000, 1, 1, 1),
+
+    ], 1 /* Default mixin of 3 before block 440,000 */);
 
     /**
      * The length of a standard address for your coin
@@ -132,7 +135,7 @@ const Configure = new function() {
     /**
      * Amount of blocks to request from the daemon at once
      */
-    this.blocksPerDaemonRequest = 50;
+    this.blocksPerDaemonRequest = 40;
 
     /**
      * User agent string
