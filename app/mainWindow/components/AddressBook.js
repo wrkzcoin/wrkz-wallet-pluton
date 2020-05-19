dc1c18d14c71109833e1dd9a4300f358561afe80
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2020-05-19 18:38:45 +0700

    Fix error build with addressbook

--- app/mainWindow/components/AddressBook.js
+++ app/mainWindow/components/AddressBook.js
@@ -9,7 +9,7 @@ import jdenticon from 'jdenticon';
 import {
   validateAddress,
   validatePaymentID
-} from 'turtlecoin-wallet-backend/dist/lib/ValidateParameters';
+} from 'turtlecoin-wallet-backend';
 import NavBar from './NavBar';
 import BottomBar from './BottomBar';
 import Redirector from './Redirector';
