161b288969879ca263670ba735609e8a1fab589e
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2020-08-20 16:38:27 +0700

    scanCoinbaseTransactions default to true

--- app/Configure.js
+++ app/Configure.js
@@ -75,7 +75,7 @@ const Configure = new function() {
      * Most people haven't mined any blocks, so lets not waste time scanning
      * them
      */
-    this.scanCoinbaseTransactions = false;
+    this.scanCoinbaseTransactions = true;
 
     /**
      * Disable AutoOptimization by default
